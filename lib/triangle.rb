cff31a2a33b31d42e0cc6e34d4001fd268b80be0
Daniel Alves de Carvalho <danielcarvalhovr@gmail.com>
2019-10-28 02:30:45 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,4 @@
+require 'pry'
 class Triangle
   def initialize(side_1,side_2,side_3)
     @side_1 = side_1
@@ -7,11 +8,7 @@ class Triangle
 
   def kind()
     if (@side_1 <= 0) || (@side_2 <= 0) || (@side_3 <= 0)
-      begin
       raise TriangleError
-    rescue TriangleError => error
-      puts error.message
-    end
     elsif (@side_1+@side_2 <= @side_3) || (@side_1+@side_3 <= @side_2) || (@side_2+@side_3 <= @side_1)
       raise TriangleError
     else
